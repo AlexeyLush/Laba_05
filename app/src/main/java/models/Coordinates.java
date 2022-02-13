4e3b31236b488b1ecfadaec84cad8ee7470cfb8d
Damdin <damdinmolonov@gmail.com>
2022-02-13 14:35:56 +0300

    Coordinates

--- app/src/main/java/models/Coordinates.java
+++ app/src/main/java/models/Coordinates.java
@@ -3,4 +3,29 @@ package models;
 public class Coordinates {
     private Long x; //Максимальное значение поля: 713, Поле не может быть null
     private int y;
+
+
+    public Coordinates(long x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // Геттеры
+    public long getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    // Сеттеры
+    public void setX(long x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    // toString
+    public String toString(){
+        return "Coordinates: {" + "x = " + x + ", y = " + y + "}";
+    }
 }
