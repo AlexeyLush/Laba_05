63ad89e1f56b680c875581fc100c81ccc6323094
Alexey Lushchenko <alexeylush4060@gmail.com>
2022-03-15 22:58:06 +0300

    Added interface MapWork

--- app/src/main/java/dao/DAO.java
+++ app/src/main/java/dao/DAO.java
@@ -12,9 +12,6 @@ public interface DAO<K extends Comparable,V extends Comparable> {
     int create(K key, V labWork);
     void update(int id, V labWork);
     void delete(K key);
-    void initialMap(Map<K, V> elements);
     void clear();
-    Map<K, V> sort(Map<K, V> map);
     V get(K key);
-    Map<K, V> getAll();
 }
