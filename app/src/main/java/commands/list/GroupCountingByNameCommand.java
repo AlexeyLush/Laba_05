ba7bef163a9f18eb41e1b22df6add1718e794079
Damdin <damdinmolonov@gmail.com>
2022-03-21 12:49:30 +0300

    команды RemoveKey FilterGreater GroupCounting

--- app/src/main/java/commands/list/GroupCountingByNameCommand.java
+++ app/src/main/java/commands/list/GroupCountingByNameCommand.java
@@ -2,6 +2,10 @@ package commands.list;
 
 import commands.CommandAbstract;
 import commands.models.CommandFields;
+import models.LabWork;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Команда группировки элементов коллекции по значению поля name, вывод количества элементов в каждой группе
@@ -16,6 +20,23 @@ public class GroupCountingByNameCommand extends CommandAbstract {
 
     @Override
     public void execute(CommandFields commandFields) {
+        Map<Character, Integer> groupByName = new LinkedHashMap<>();
+        try{
+            for (Map.Entry<String, LabWork> entry : commandFields.getLabWorkDAO().getAll().entrySet()) {
+                Character firstChar = entry.getValue().getName().toCharArray()[0];
+                if (!groupByName.containsKey(firstChar)){
+                    groupByName.put(firstChar, 0);
+                }
+                groupByName.put(firstChar, groupByName.get(firstChar) + 1);
 
+            }
+
+            for (Map.Entry<Character, Integer> entry : groupByName.entrySet()) {
+                commandFields.getConsoleManager().outputln(String.format("Кол-во названий, начинающихся с '%c': %d",entry.getKey(), entry.getValue()));
+            }
+
+        } catch (NullPointerException nullPointerException){
+            commandFields.getConsoleManager().error("Ошибка!");
+        }
     }
 }
